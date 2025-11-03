bbdbf68fa146a5c613af8b261c0a94177fead29d
Administrator <gitlab_admin_51b600@example.com>
2025-11-02 18:53:02 -0600

    Add decades calculation script with user input and detailed documentation

--- number1.py
+++ number1.py
@@ -1,7 +1,11 @@
-number = 10
-print("The number is:", number)
-print("The type of number is:", type(number))
-number2 = float(number)
+"""
+This script demonstrates basic number manipulation in Python.
+It initializes an integer, converts it to a float, and then back to an integer,
+"""
+number1 = 10
+print("The number is:", number1)
+print("The type of number is:", type(number1))
+number2 = float(number1)
 print("The number converted to float is:", number2)
 print("The type of number is:", type(number2))
 number3 = int(number2)
