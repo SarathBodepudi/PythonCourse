8c656ecf11b9297d8ece2a8cfa87ea7fa1255f0b
Administrator <gitlab_admin_51b600@example.com>
2026-02-17 18:43:50 -0600

    sequences

--- fundamentals/sequence_start.py
+++ fundamentals/sequence_start.py
@@ -3,19 +3,38 @@
 
 # Sequences: Lists and Tuples
 # These are -- surprise -- sequences of values
+mylist = [1, 2, 3, 4, 5]
+print(mylist)
+print(len(mylist))
 
+mylist = [1, 2, 3, "four", "five", 6.0]
+print(mylist)
 
-# to access a member of a sequence type, use []
+print(mylist[0])  # access the first element
+print(mylist[3])  # access the fourth element
+print(mylist[-1])  # access the last element
+#print(mylist[-10])
+print(mylist[1:4])  # access a slice of the list (from index 1 to 3)
+print(mylist[:3])  # access the first three elements
+print(mylist[3:])  # access from the fourth element to the end
+
+print("==========================")  # access every second element
+print(mylist[::2])  # access every second element
 
 
 # add a list to another list
-
+mylist = [1, 2, 3, "four", "five", 6.0]
+mylist2 = [7, 8, 9]
+mylist.extend(mylist2)
+print(mylist)
+print(mylist + mylist2)  # this creates a new list that is the concatenation of the two lists
 
 # use slices to get parts of a sequence
 
 
 # you can use slices to reverse a sequence
-
+print("============ reverse string ==============")
+print(mylist[::-1])  # this creates a new list that is the reverse of the original list
 
 # Tuples are like lists, but they are immutable
 
--- fundamentals/strings1.py
+++ fundamentals/strings1.py
@@ -1,3 +1,7 @@
+# Strings are sequences of characters, 
+# and they are immutable (cannot be changed after they are created)
+
+
 # my_name = input("Enter your name: ")
 # greeting = "Hello, " + my_name + "!"
 # print(greeting)
@@ -13,3 +17,9 @@ print(my_int)
 
 my_int = "hello world"
 print(my_int)
+
+# string is a sequence of characters, so we can use indexing and slicing to access parts of the string
+print(string1[0])  # access the first character
+
+# reverse the string using slicing
+print(string1[::-1])
