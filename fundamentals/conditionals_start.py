6dad26ed9914d79e99a549cb442001df1afeb2cc
Administrator <gitlab_admin_51b600@example.com>
2026-02-24 17:51:56 -0600

    dictionary and conditionals

---
+++ fundamentals/conditionals_start.py
@@ -0,0 +1,15 @@
+# LinkedIn Learning Python course by Joe Marini
+# Example file for working with conditional statements
+
+
+x, y = 10, 100
+
+# conditional flow uses if, elif, else
+if x < y:
+    print("x is less than y")
+    print("this is another statement in the if block")
+elif x == y:
+    print("x and y are equal")  
+else:
+    print("x is greater than y")
+# conditional statements let you use "a if C else b"
