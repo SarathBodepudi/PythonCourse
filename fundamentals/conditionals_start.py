3ea28f03e860eab02e50374360261ec33b9858a8
Administrator <gitlab_admin_51b600@example.com>
2026-02-24 18:22:59 -0600

    loops and enumerations

--- fundamentals/conditionals_start.py
+++ fundamentals/conditionals_start.py
@@ -13,3 +13,6 @@ elif x == y:
 else:
     print("x is greater than y")
 # conditional statements let you use "a if C else b"
+print("---------------------------------")
+message = "x is less than y" if x < y else "x is greater than or equal to y"  # pylint: disable=invalid-name
+print(message)
